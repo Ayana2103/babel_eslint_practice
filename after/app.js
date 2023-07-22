538cc212f48f31fdd767099feadfe85d768c78d0
ayana <ayana.s.prose@gmail.com>
2023-07-22 22:16:13 +0900

    submission

---
+++ after/app.js
@@ -0,0 +1,118 @@
+$(function () {
+  // pageCountに1を代入
+  let pageCount = 1;
+  // keepWordを空にする
+  let keepWord = '';
+
+  // 検索ボタンを押してイベント実行
+  $('.search-btn').on('click', function (){
+    // 検索ワードを取得
+    const searchWord =$('#search-input').val();
+    // sarchWordがkeepWordと一致する場合
+    if( searchWord === keepWord ){
+    // pageCountに＋1
+    pageCount++;
+    } else {
+    // 一致しない場合listの要素中身を空にする
+    $('.lists').empty();
+    // keepWordにsearchWordの値を代入
+    keepWord = searchWord;
+    pageCount = 1;
+    }
+
+
+  // 変数settingsに設定情報などを格納
+  const settings = {
+    // 実⾏するURL(エンドポイント)
+    'url': `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
+    // サーバーに送るメソッド
+    'method': 'GET'
+  };
+  // Ajaxの実⾏
+  $.ajax(settings).done(function (response) {
+    // 通信成功した時の処理。引数responseが通信した結果を受け取っている
+    // resultにrespons['@graph']を格納
+    const result = response['@graph'];
+    // 関数displayResultに引数resultを渡して実行
+    displayResult(result);
+  // 通信に失敗した時の処理。errにサーバーから送られてきたエラー内容を受け取っている
+  }).fail(function (err) {
+    // 関数displayErrorに引数errを渡して実行
+    displayError(err);
+  });
+});
+
+
+  // 関数displayResultに仮引数resultを渡す
+  function displayResult(result) {
+    // message要素を削除
+    $('.message').remove();
+    // result[0]['items']がundefinedでない場合
+    if(result[0].items !== undefined ){
+      // ajaxから取得したアイテムの数分ループする
+      $.each(result[0].items, function(i){
+        // titleに値が返ってきた場合その値（true）、空だった場合はタイトル不明（false）を代入※creator,publisher,idもそれぞれ同様に代入
+        const title =result[0].items[i].title
+        ? result[0].items[i].title
+        : 'タイトル不明';
+        const creator = result[0].items[i]['dc:creator']
+        ? result[0].items[i]['dc:creator']
+        : '作者不明';
+        const publisher = result[0].items[i]['dc:publisher']
+        ? result[0].items[i]['dc:publisher']
+        : '出版社不明';
+        const id = result[0].items[i]['@id']
+        ? result[0].items[i]['@id']
+        : '書籍情報不明';
+        // listsクラスの子要素の先頭に上記で代入したタイトル、作者、出版社、書籍情報詳細に飛ぶリンク要素を追加
+        $('.lists').prepend('<li class="lists-item"><div class="list-inner"><p>タイトル：' + title + '</p><p>作者：' + creator + '</p><p>出版社：' + publisher + '</p><a href = "' + id + '"target="_blank">書籍情報</a></div></li>');
+      });
+
+
+    } else {
+      // undifinedの場合、listsクラスを持つ要素の前に下記テキストを追加
+      $('.lists').before('<div class="message">検索結果が見つかりませんでした<br>別のキーワードで検索してください</div>');
+    }
+  }
+
+
+  // 関数名displayErrorに仮引数errを渡す
+  function displayError(err){
+    // lists要素の中を空にする
+    $('.lists').empty();
+    // message要素を削除
+    $('.message').remove();
+
+    // err.statusが0の場合
+    if(err.status === 0) {
+      // lists要素の前に下記divを追加
+      $('.lists').before('<div class="message">正常に通信できませんでした。<br>インターネットの接続を確認してください。</div>');
+
+    // err.statusが400の場合
+    } else if (err.status === 400) {
+      // lists要素の前に下記divを追加
+      $('.lists').before('<div class="message">検索ワードが入力されていません。</div>');
+
+    // それ以外の場合
+    } else {
+       // lists要素の前に下記divを追加
+      $('.lists').before('<div class="message">予期せぬエラーが発生しました。</div>');
+    }
+  }
+
+
+  // リセットボタンを押して下記内容の実行
+  $('.reset-btn').on('click', function(){
+    // ページカウントを１にする
+    pageCount = 1;
+    // list要素の中を空にする
+    $('.lists').empty();
+    // 入力フォームの中を空にする
+    $('#search-input').val('');
+    // massage要素を削除
+    $('.massage').remove();
+    // keepWordを空にする
+    keepWord = '';
+  });
+});
+
